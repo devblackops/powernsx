4327db283faa4281787a2c6ad99ff9da13786e86
Brandon Olin <brandon@devblackops.io>
2016-09-11 12:18:05 -0700

    Add Pester test framework files

---
+++ psake.ps1
@@ -0,0 +1,87 @@
+properties {
+    $projectRoot = $ENV:BHProjectPath
+    if(-not $projectRoot) {
+        $projectRoot = $PSScriptRoot
+    }
+
+    $sut = "$projectRoot\$env:PHProjectName"
+    $metaTests = "$projectRoot\Tests\meta"
+    $unitTests = "$projectRoot\Tests\unit"
+    $integrationTests = "$projectRoot\Tests\integration"
+    $tests = "$projectRoot\Tests"
+
+    $psVersion = $PSVersionTable.PSVersion.Major
+}
+
+task default -depends Test
+
+task Init {
+    "`nSTATUS: Testing with PowerShell $psVersion"
+    "Build System Details:"
+    Get-Item ENV:BH*
+
+    $modules = 'Pester', 'PSDeploy', 'PSScriptAnalyzer'
+    Install-Module $modules -Confirm:$false
+    Import-Module $modules -Verbose:$false -Force
+}
+
+task Test -Depends Init, Analyze, Pester-Meta, Pester-Module
+
+task Analyze -Depends Init {
+    $saResults = Get-ChildItem -File -Path $sut -Exclude '*.tests.ps1' -Recurse | 
+        Invoke-ScriptAnalyzer -Severity Error
+    if ($saResults) {
+        $saResults | Format-Table
+        Write-Error -Message 'One or more Script Analyzer errors/warnings where found. Build cannot continue!'
+    }
+}
+
+task Pester-Meta -Depends Init {   
+    if(-not $ENV:BHProjectPath) {
+        Set-BuildEnvironment -Path $PSScriptRoot\..
+    }
+    Remove-Module $ENV:BHProjectName -ErrorAction SilentlyContinue
+    Import-Module (Join-Path $ENV:BHProjectPath $ENV:BHProjectName) -Force
+
+    $testResults = Invoke-Pester -Path $metaTests -PassThru
+    if ($testResults.FailedCount -gt 0) {
+        $testResults | Format-List
+        Write-Error -Message 'One or more Pester tests failed. Build cannot continue!'
+    }
+}
+
+task Pester-Module -depends Init {
+    if(-not $ENV:BHProjectPath) {
+        Set-BuildEnvironment -Path $PSScriptRoot\..
+    }
+    Remove-Module $ENV:BHProjectName -ErrorAction SilentlyContinue
+    Import-Module (Join-Path $ENV:BHProjectPath $ENV:BHProjectName) -Force
+
+    $testResults = Invoke-Pester -Path $unitTests -PassThru
+    if ($testResults.FailedCount -gt 0) {
+        $testResults | Format-List
+        Write-Error -Message 'One or more Pester tests failed. Build cannot continue!'
+    }
+    Remove-Module $ENV:BHProjectName -ErrorAction SilentlyContinue
+}
+
+task Deploy -depends Test, GenerateHelp {
+    # Gate deployment
+    if( $ENV:BHBuildSystem -ne 'Unknown' -and
+        $ENV:BHBranchName -eq "master" -and
+        $ENV:BHCommitMessage -match '!deploy'
+    ) {
+        $params = @{
+            Path = "$projectRoot\module.psdeploy.ps1"
+            Force = $true
+            Recurse = $false
+        }
+
+        Invoke-PSDeploy @Params
+    } else {
+        "Skipping deployment: To deploy, ensure that...`n" +
+        "`t* You are in a known build system (Current: $ENV:BHBuildSystem)`n" +
+        "`t* You are committing to the master branch (Current: $ENV:BHBranchName) `n" +
+        "`t* Your commit message includes !deploy (Current: $ENV:BHCommitMessage)"
+    }
+}
