5810df37c618aba6a9f19f6485db43dabdfbced1
Brandon Olin <brandon@devblackops.io>
2016-09-11 12:56:55 -0700

    Remove psake task for module deployment

--- psake.ps1
+++ psake.ps1
@@ -64,24 +64,3 @@ task Pester-Module -depends Init {
     }
     Remove-Module $ENV:BHProjectName -ErrorAction SilentlyContinue
 }
-
-task Deploy -depends Test, GenerateHelp {
-    # Gate deployment
-    if( $ENV:BHBuildSystem -ne 'Unknown' -and
-        $ENV:BHBranchName -eq "master" -and
-        $ENV:BHCommitMessage -match '!deploy'
-    ) {
-        $params = @{
-            Path = "$projectRoot\module.psdeploy.ps1"
-            Force = $true
-            Recurse = $false
-        }
-
-        Invoke-PSDeploy @Params
-    } else {
-        "Skipping deployment: To deploy, ensure that...`n" +
-        "`t* You are in a known build system (Current: $ENV:BHBuildSystem)`n" +
-        "`t* You are committing to the master branch (Current: $ENV:BHBranchName) `n" +
-        "`t* Your commit message includes !deploy (Current: $ENV:BHCommitMessage)"
-    }
-}
